036a9d86d1f5cf3ea1e34ba72116dedca80e9e9f
caster-k <castergastino@gmail.com>
2024-10-01 13:56:46 +0545

    final-version of quiz using multithread!

--- test_quiz_multithread.cpp
+++ test_quiz_multithread.cpp
@@ -5,23 +5,24 @@
 #include <thread>
 #include <cstdlib>
 #include <ctime>
-#include <cstring> 
+#include <cstring>
 #include <atomic>
 
 using namespace std;
 
-// Shared atomic variables for user input and time extension tracking
+// Shared atomic variable for user input
 atomic<char> option(' ');
-atomic<bool> time_extended(false);
 
 void inputHandler() {
     while (true) {
         char input;
+        this_thread::sleep_for(chrono::seconds(3));
         cin >> input;
-        option = input;
-        if(option >='a' && option <= 'z') {
-            option -= 32; // Convert lowercase to uppercase
+        if (input >= 'a' && input <= 'z') {
+            input -= 32; // Convert lowercase to uppercase
         }
+        option = input;
+        
     }
 }
 
@@ -31,10 +32,10 @@ int main() {
     MYSQL_ROW row;
     MYSQL_RES *res_a;
     MYSQL_ROW row_a;
-    const char *server = "localhost"; // Change to your server address
-    const char *user = "root"; // Change to your MySQL username
-    const char *password = ".."; // Change to your MySQL password
-    const char *database = "QUIZ"; // Change to your database name
+    const char *server = "localhost";
+    const char *user = "root";
+    const char *password = "Sunta@123...";
+    const char *database = "QUIZ";
 
     // Initialize MySQL connection
     conn = mysql_init(NULL);
@@ -45,29 +46,23 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    // Execute SQL query
     srand(time(0));
     int qno[5] = {0};
     int c = 0;
+
     while (c < 5) {
         int r = (rand() % 5) + 1;
-        int flag = 0;
-        for (int j = 0; j < 5; j++) {
+        bool exists = false;
+        for (int j = 0; j < c; j++) {
             if (qno[j] == r) {
-                flag = 1;
-                break;  
+                exists = true;
+                break;
             }
         }
-        if (flag == 0) {
-            qno[c] = r;
-            c++;
+        if (!exists) {
+            qno[c++] = r;
         }
     }
-
-    for (int i = 0; i < 5; i++) {
-        cout << qno[i] << " ";
-    }
-    cout << endl;
 
     // Start input thread
     thread inputThread(inputHandler);
@@ -75,21 +70,17 @@ int main() {
     for (int i = 0; i < 5; i++) {
         // Fetch question
         string query = "SELECT * FROM questions WHERE Qno = " + to_string(qno[i]);
-        cout << "Executing query: " << query << endl;
-        
         if (mysql_query(conn, query.c_str())) {
             cout << "Error executing query: " << mysql_error(conn) << endl;
             return EXIT_FAILURE;
         }
 
-        // Store result for questions
         res = mysql_store_result(conn);
         if (res == NULL) {
             cout << "mysql_store_result() failed. Error: " << mysql_error(conn) << endl;
-            return EXIT_FAILURE;
+            continue; // Move to next question
         }
 
-        // Fetch question row
         row = mysql_fetch_row(res);
         if (row == NULL) {
             cout << "No question found for Qno: " << qno[i] << endl;
@@ -99,22 +90,19 @@ int main() {
 
         // Fetch answers
         string query_a = "SELECT * FROM answers WHERE Qno = " + to_string(qno[i]);
-        cout << "Executing query: " << query_a << endl;
         if (mysql_query(conn, query_a.c_str())) {
             cout << "Error executing query: " << mysql_error(conn) << endl;
             mysql_free_result(res);
             return EXIT_FAILURE;
         }
 
-        // Store result for answers
         res_a = mysql_store_result(conn);
         if (res_a == NULL) {
             cout << "mysql_store_result() failed. Error: " << mysql_error(conn) << endl;
             mysql_free_result(res);
-            return EXIT_FAILURE;
+            continue; // Move to next question
         }
 
-        // Fetch answer row
         row_a = mysql_fetch_row(res_a);
         if (row_a == NULL) {
             cout << "No answers found for Qno: " << qno[i] << endl;
@@ -124,9 +112,7 @@ int main() {
         }
 
         // Countdown and input handling
-        int k = 59;
-        while (k >= 0) {
-             // Display question and options
+        for (int k = 60; k >= 0; k--) {
             system("clear");
             cout << "Question: " << qno[i] << " " << (row[1] ? row[1] : "NULL") << endl;
             cout << "A. " << (row[2] ? row[2] : "NULL") << endl; 
@@ -134,44 +120,35 @@ int main() {
             cout << "C. " << (row[4] ? row[4] : "NULL") << endl; 
             cout << "D. " << (row[5] ? row[5] : "NULL") << endl;
             cout << "Enter your choice (A/B/C/D): ";
-            cout << " \t\t\t\t\t\t\t\t\t\t\t\t\t\t\tseconds remaining: "<<k << endl;
+            cout << "\t\tseconds remaining: " << k << endl;
+
             this_thread::sleep_for(chrono::seconds(1));
 
-            // Handle user input every 20 seconds
-             // If the user has given input, break the loop
             if (option != ' ') {
                 cout << "You chose: " << option << endl;
-                this_thread::sleep_for(chrono::seconds(5));
+                if (strcmp(row_a[1], string(1, option).c_str()) == 0) {
+                    cout << "Your answer is correct!" << endl;
+                    this_thread::sleep_for(chrono::seconds(3));
+                } else {
+                    cout << "Your answer is incorrect!" << endl;
+                    this_thread::sleep_for(chrono::seconds(3));
+                }
+                option = ' '; // Reset option for next question
                 break;
             }
-            else if (strcmp(row_a[1], string(1, option).c_str()) == 0) {
-                    cout <<endl<< "Your answer is correct!" << endl;
-                    // break;
-                }
-            else{
-                    cout <<endl<< "Your answer is incorrect!" << endl;
-                    // break;
-                }
-            // If time runs out or user selects an option, proceed to next question
+
             if (k == 0) {
                 cout << "Time's up for this question!" << endl;
             }
-            // Reset option for the next question
-            option = ' ';
-                k--;
+            
         }
-        cout << "NEXT QUESTION" << endl;
-        this_thread::sleep_for(chrono::seconds(3));
 
-        // Cleanup for each question and answers
-        // mysql_free_result(res);
         mysql_free_result(res);
         mysql_free_result(res_a);
     }
 
-    // Final cleanup
     mysql_close(conn);
-    inputThread.detach();
+    inputThread.join(); // Wait for the input thread to finish
 
     return EXIT_SUCCESS;
 }
