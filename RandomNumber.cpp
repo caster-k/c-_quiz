aac244a72073674d48d1b21d952a5ab6a7a51471
caster-k <castergastino@gmail.com>
2024-10-04 00:02:02 +0545

    backup

--- RandomNumber.cpp
+++ RandomNumber.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int unq[5]={0};
+    int unq[46]={0};
     int c = 0;
-    while (c < 5)
+    while (c < 45)
     {
-        int r = rand() % 10;
+        int r = rand() % 45;
         int flag = 0;
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < 45; j++)
         {
-            printf("%d ", unq[j]);
+            // printf("%d ", unq[j]);
             if (unq[j] == r)
             {
                     flag = 1;
@@ -21,14 +21,14 @@ int main()
             }
            
         }
-         printf("\n");
-        printf("\n");
+        //  printf("\n");
+        // printf("\n");
         if(flag == 0){
         unq[c] = r;
         c++;
         }
     }
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 45; i++)
     {
         printf("%d ", unq[i]);
     }
